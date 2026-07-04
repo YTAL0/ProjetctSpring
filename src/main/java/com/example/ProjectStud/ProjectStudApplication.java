1ae9da01625c037537d4122748ccecb87f5a3787
YTAL0 <ytalo122@hotmail.com>
2026-07-03 22:43:45 -0300

    firts commit

---
+++ src/main/java/com/example/ProjectStud/ProjectStudApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectStud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStudApplication.class, args);
+	}
+
+}
