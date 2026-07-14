a8f6370989a400c8d436dae09b32b0a6553f8efa
YTAL0 <ytalo122@hotmail.com>
2026-07-13 23:50:09 -0300

    Change for mavem

---
+++ projectEstud/src/main/java/com/estudos/projectEstud/ProjectEstudApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.projectEstud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEstudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEstudApplication.class, args);
+	}
+
+}
