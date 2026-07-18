3152ee78cf0324b676ab46d2b6ccc3d302d2bb2a
YTAL0 <ytalo122@hotmail.com>
2026-07-17 22:03:04 -0300

    Refactor

--- projectEstud/src/main/java/com/estudos/projectEstud/model/Person.java
+++ projectEstud/src/main/java/com/estudos/projectEstud/model/Person.java
@@ -1,10 +1,18 @@
 package com.estudos.projectEstud.model;
 
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 @Entity
 @Table(name = "person")
 public class Person implements Serializable {
@@ -12,7 +20,6 @@ public class Person implements Serializable {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long ID;
-
    @Column(name = "first_name", nullable = false, length = 80)
     private String name;
     @Column(name = "last_name", nullable = false, length = 80)
@@ -21,68 +28,5 @@ public class Person implements Serializable {
     private String gender;
     @Column(nullable = false, length = 100)
     private String adress;
-    public Person() {
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public void setID(long ID) {
-        this.ID = ID;
-    }
-
-    public void setAdress(String adress) {
-        this.adress = adress;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public long getID() {
-        return ID;
-    }
-
-    public String getAdress() {
-        return adress;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof Person person)) return false;
-        return ID == person.ID && Objects.equals(name, person.name) && Objects.equals(lastName, person.lastName) && Objects.equals(gender, person.gender) && Objects.equals(adress, person.adress);
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", gender='" + gender + '\'' +
-                ", ID=" + ID +
-                ", adress='" + adress + '\'' +
-                '}';
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, lastName, gender, ID, adress);
-    }
 }
